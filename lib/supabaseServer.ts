6ed01c0e660b65ca487fa6f9972701e5ebd0e9a8
WenSquareEJ <wenyu.yan@gmail.com>
2025-10-02 13:10:15 +0100

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabaseServer = () =>
-  createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
-    auth: { persistSession: false }
-  })
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '@/lib/database.types' // optional if you generated types
+
+export const supabaseServer = () => {
+  return createServerComponentClient<Database>({ cookies })
+}
