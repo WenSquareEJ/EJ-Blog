ed203ae13a06d2d7c163035b46d9de959552c052
WenSquareEJ <wenyu.yan@gmail.com>
2025-10-03 07:45:32 +0100

    Update supabaseServer.ts

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,7 +1,25 @@
-// lib/supabaseServer.ts
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+// /lib/supabaseServer.ts
+import { createServerClient, type CookieOptions } from "@supabase/ssr"
+import { cookies } from "next/headers"
 
-export const supabaseServer = () => {
-  return createServerComponentClient({ cookies })
+export function supabaseServer() {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options })
+        },
+      },
+    }
+  )
 }
