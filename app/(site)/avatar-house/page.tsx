b3379ce8f26f24c724f1b06de7dac308fa62487e
WenSquareEJ <wenyu.yan@gmail.com>
2025-10-05 21:01:11 +0100

    test ava

--- app/(site)/avatar-house/page.tsx
+++ app/(site)/avatar-house/page.tsx
@@ -1,25 +1,8 @@
 
 
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
-import AvatarHousePageClient from "./AvatarHousePageClient";
-
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default async function Page() {
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: cookies() }
-  );
-
-  const { data: { user } } = await supabase.auth.getUser();
-
-  if (!user) {
-    redirect("/login?next=/avatar-house");
-  }
-
-  return <AvatarHousePageClient initialUser={user} />;
+export default function Page() {
+  return <div style={{padding:20}}>Avatar route is alive</div>;
 }
